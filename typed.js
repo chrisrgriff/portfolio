411a480760c9fc5e78b3c38bd4ed6a967d09960c
Christopher Griffith <chrisrgriff@gmail.com>
2019-07-26 21:05:31 -0400

    july-26

---
+++ typed.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+  Typed.new('.type', {
+    strings: ["Business Strategist", "Digital Marketer", "UX Designer", "User Researcher", "Problem Solver"],
+    stringsElement: null,
+    typeSpeed: 60,
+    startDelay: 0,
+    backSpeed: 10,
+    smartBackspace: true,
+    backDelay: 500,
+    loop: false,
+    loopCount: 1,
+    showCursor: true,
+    cursorChar: "|",
+    attr: null,
+    contentType: 'html',
+  });
+});
